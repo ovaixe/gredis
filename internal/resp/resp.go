61ca5d0be510dbf5b48c1ecba1fdb6a954c65ffa
Bhat Owais <owaisbhat996@outlook.com>
2024-11-05 22:13:11 +0530

    Updated resp > added writer

--- internal/resp/resp.go
+++ internal/resp/resp.go
@@ -31,8 +31,27 @@ type Resp struct {
 	reader *bufio.Reader
 }
 
+type Writer struct {
+	writer io.Writer
+}
+
 func NewResp(rd io.Reader) *Resp {
 	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{writer: w}
+}
+
+func (w *Writer) Write(v Value) error {
+	var bytes = v.Marshal()
+	
+	_, err := w.writer.Write(bytes)
+	if err != nil {
+		return err
+	}
+	
+	return nil
 }
 
 // readLine reads the line from the buffer.
@@ -72,28 +91,28 @@ func (r *Resp) Read() (Value, error) {
 	if err != nil {
 		return Value{}, err
 	}
-	
+
 	switch _type {
-		case ARRAY:
-			return r.readArray()
-		case BULK:
-			return r.readBulk()
-		default:
-			fmt.Printf("Unknown type: %v", string(_type))
-			return Value{}, nil
+	case ARRAY:
+		return r.readArray()
+	case BULK:
+		return r.readBulk()
+	default:
+		fmt.Printf("Unknown type: %v", string(_type))
+		return Value{}, nil
 	}
 }
 
 func (r *Resp) readArray() (Value, error) {
 	v := Value{}
 	v.typ = "array"
-	
+
 	// read length of array
 	len, _, err := r.readInteger()
 	if err != nil {
 		return v, err
 	}
-	
+
 	// for each line, parse and read the value
 	v.array = make([]Value, 0)
 	for i := 0; i < len; i++ {
@@ -101,29 +120,94 @@ func (r *Resp) readArray() (Value, error) {
 		if err != nil {
 			return v, err
 		}
-		
+
 		// append parsed value to the array
 		v.array = append(v.array, val)
 	}
-	
+
 	return v, nil
 }
 
 func (r *Resp) readBulk() (Value, error) {
 	v := Value{}
 	v.typ = "bulk"
-	
+
 	len, _, err := r.readInteger()
 	if err != nil {
 		return v, err
 	}
-	
+
 	bulk := make([]byte, len)
 	r.reader.Read(bulk)
 	v.bulk = string(bulk)
-	
+
 	// read the trailing CRLF
 	r.readLine()
-	
+
 	return v, nil
+}
+
+func (v Value) Marshal() []byte {
+	switch v.typ {
+	case "array":
+		return v.marshalArray()
+	case "bulk":
+		return v.marshalBulk()
+	case "string":
+		return v.marshalString()
+	case "null":
+		return v.marshalNull()
+	case "error":
+		return v.marshalError()
+	default:
+		return []byte{}
+
+	}
+}
+
+func (v Value) marshalString() []byte {
+	var bytes []byte
+	bytes = append(bytes, STRING)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	
+	return bytes
+}
+
+func (v Value) marshalBulk() []byte {
+	var bytes []byte
+	bytes = append(bytes, BULK)
+	bytes = append(bytes, strconv.Itoa(len(v.bulk))...)
+	bytes = append(bytes, '\r', '\n')
+	bytes = append(bytes, v.bulk...)
+	bytes = append(bytes, '\r', '\n')
+	
+	return bytes
+}
+
+func (v Value) marshalArray() []byte {
+	len := len(v.array)
+	var bytes []byte
+	bytes = append(bytes, ARRAY)
+	bytes = append(bytes, strconv.Itoa(len)...)
+	bytes = append(bytes, '\r', '\n')
+	
+	for i := 0; i < len; i++ {
+		bytes = append(bytes, v.array[i].Marshal()...)
+	}
+	
+	return bytes
+}
+
+func (v Value) marshalError() []byte {
+	var bytes []byte
+	bytes = append(bytes, ERROR)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	
+	return bytes
+}
+
+func (v Value) marshalNull() []byte {
+	return []byte("$-1\r\n")
 }
